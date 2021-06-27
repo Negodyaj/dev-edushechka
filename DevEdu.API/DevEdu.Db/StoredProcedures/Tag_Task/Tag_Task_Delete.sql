676229bd725ef4b06be4eebacbac3874458b8c62
Katerina <k.panteleewa@gmail.com>
2021-06-27 21:14:22 +0300

    Added stored procedure Insert and delete for Tag_Task

---
+++ DevEdu.API/DevEdu.Db/StoredProcedures/Tag_Task/Tag_Task_Delete.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[Tag_Task_Delete]
+	@TagID int ,
+	@TaskID int
+AS
+	Delete from [dbo].[Tag_Task] 
+	Where TagId = @TagID and TaskID = @TaskID
---
+++ DevEdu.API/DevEdu.Db/StoredProcedures/Tag_Task/Tag_Task_Insert.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[Tag_Task_Insert]
+	@TagID int ,
+	@TaskID int
+AS
+	Insert into [dbo].[Tag_Task] ([TagId],[TaskId])
+	Values (@TagID, @TaskID)
