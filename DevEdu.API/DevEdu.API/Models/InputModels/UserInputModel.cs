a4073ad35942e082b2a9e0589f3408ca9255c857
Eugene <Zhekul.90@gmail.com>
2021-06-29 02:50:00 +0300

    add UserInputModel

---
+++ DevEdu.API/DevEdu.API/Models/InputModels/UserInputModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevEdu.API.Models.InputModels
+{
+    public class UserInputModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ContractNumber { get; set; }
+        [Required]
+        public int CityId { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        public string GitHubAccount { get; set; }
+        [Required]
+        public string Photo { get; set; }
+        [Required]
+        public string PhoneNumer { get; set; }
+
+    }
+}
