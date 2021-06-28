f145f8b7c01fc460853d7dda9f883e8025c6dd99
MariaMalyutina <merymal2696@gmail.com>
2021-06-28 14:34:08 +0300

    Added CRUD procedures for Material

---
+++ DevEdu.API/DevEdu.Db/StoredProcedures/Material/Material_Insert.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[Material_Insert]
+@Content nvarchar(max)
+AS
+INSERT INTO [Material] (Content)
+VALUES (@Content)
+SELECT @@IDENTITY
---
+++ DevEdu.API/DevEdu.Db/StoredProcedures/Material/Material_Update.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Material_Update]
+@Id int,
+@Content nvarchar(max)
+AS
+UPDATE [Material]
+SET
+[Content] = @Content
+WHERE [Id] = @Id
