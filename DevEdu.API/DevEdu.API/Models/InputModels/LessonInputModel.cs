0acb6818c5ff034cfe89a2605ad922fa5ef971c6
Nastya <kushkoeva99@gmail.com>
2021-06-30 13:30:11 +0300

    add LessonInputModel

---
+++ DevEdu.API/DevEdu.API/Models/InputModels/LessonInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEdu.API.Models.InputModels
+{
+    public class LessonInputModel
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string TeacherComment { get; set; }
+        [Required]
+        public int TeacherId { get; set; }
+    }
+}
