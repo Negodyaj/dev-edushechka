0b21d423cc55fbd306a1ed6064cf964734a79b7d
MariaMalyutina <merymal2696@gmail.com>
2021-06-30 17:16:45 +0300

    Auto stash before rebase of "origin/main"

---
+++ DevEdu.API/DevEdu.API/Controllers/MaterialController.cs
@@ -0,0 +1,20 @@
+﻿using DevEdu.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEdu.API.Controllers
+{
+    public class MaterialController : Controller
+    {
+        public MaterialController() { }
+
+        [HttpPost]
+        public int AddMaterial([FromBody] MaterialInputModel material)
+        {
+            return 42;
+        }
+    }
+}
