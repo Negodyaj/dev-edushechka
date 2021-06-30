489a9901aa5b0c0abc8add916d87508a6edd38ea
Eugene <Zhekul.90@gmail.com>
2021-06-30 16:38:55 +0300

    add User Http request

--- DevEdu.API/DevEdu.API/Controllers/UserController.cs
+++ DevEdu.API/DevEdu.API/Controllers/UserController.cs
@@ -1,16 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DevEdu.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevEdu.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/user")]
     public class UserController : Controller
     {
+        UserInputModel model = new UserInputModel();
+
         public UserController() { }
+
+        [HttpPost]
+        public int AddUser([FromBody] UserInputModel model)
+        {
+            return 1;
+        }
+
+        [HttpGet("{UserId}")]
+        public int GetUserById(int UserId)
+        {
+
+            return UserId;
+        }
+
+        [HttpDelete("{UserId}")]
+        public string DeleteUser(int UserId)
+        {
+            return $"User with number {UserId} is deleted";
+        }
 
         // api/user/{userId}/role/2
         [HttpPost("{userId}/role/{roleId}")]
