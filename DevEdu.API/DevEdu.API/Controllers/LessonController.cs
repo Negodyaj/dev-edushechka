793091a8b8bc69a87aef306201e8329588c6623d
Sergey <tootoo9723@gmail.com>
2021-07-01 21:27:59 +0300

    UpdateLessonController and add UpdateInputModels for StudetntLesson

--- DevEdu.API/DevEdu.API/Controllers/LessonController.cs
+++ DevEdu.API/DevEdu.API/Controllers/LessonController.cs
@@ -57,5 +57,45 @@ namespace DevEdu.API.Controllers
         {
             return $"lessonId {lessonId} topicId {topicId}";
         }
+
+
+
+
+        // api/lesson/userId/{userId}/lessonId/{lessonId} 
+        [HttpPost("userId/{userId}/lessonId/{lessonId} ")]
+        public string AddStudenToLesson(int userId, int lessonId)
+        {
+            return $"userId {userId} lessonId {lessonId} ";
+        }
+
+
+           // api/lesson/userId/{userId}/lessonId/{lessonId} 
+        [HttpDelete("userId/{userId}/lessonId/{lessonId} ")]
+        public string DeleteStodentFromLesson(int userId, int lessonId)
+        {
+            return $"userId {userId} lessonId {lessonId} ";
+        }
+
+
+        // api/lesson/userId/{userId}/lessonId/{lessonId}/feedback 
+        [HttpPut("userId/{userId}/lessonId/{lessonId}/feedback ")]
+        public string UpdateFeedbackOfStudenLesson(int userId, int lessonId, [FromBody] StudentLessonUpdateFeedbackInputModel inputModel)
+        {
+            return $"userId {userId} lessonId {lessonId}, feedback {inputModel.Feedback}  ";
+        }
+
+        // api/lesson/userId/{userId}/lessonId/{lessonId}/absenceReason
+        [HttpPut("userId/{userId}/lessonId/{lessonId}/absenceReason ")]
+        public string UpdateAbsenceReasonOfStudenLesson(int userId, int lessonId, [FromBody] StudentLessonUpdateAbsenceReasonInputModel inputModel)
+        {
+            return $"userId {userId} lessonId {lessonId},absenceReason {inputModel.AbsenceReason}  ";
+        }
+
+        // api/lesson/userId/{userId}/lessonId/{lessonId}/isPresent
+        [HttpPut("userId/{userId}/lessonId/{lessonId}/isPresent ")] 
+        public string UpdateIsPresentOfStudenLesson(int userId, int lessonId, [FromBody] StudentLessonUpdateIsPresentInputModel inputModel)
+        {
+            return $"userId {userId} lessonId {lessonId},isPresent {inputModel.IsPresent} ";
+        }
     }
 }
---
+++ DevEdu.API/DevEdu.API/Models/InputModels/StudentLessonUpdateAbsenceReasonInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEdu.API.Models.InputModels
+{
+    public class StudentLessonUpdateAbsenceReasonInputModel
+    {
+        [Required]
+        public string AbsenceReason { get; set; }
+    }
+}
