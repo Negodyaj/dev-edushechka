698c7976a1dd778524086d990d45399c26499aff
Vadim <vki-13@yandex.ru>
2021-07-02 15:20:37 +0300

    Added TagMapping

---
+++ DevEdu.API/DevEdu.API/Mappings/TagMapping.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using DevEdu.API.Models.InputModels;
+using DevEdu.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEdu.API.Mappings
+{
+    public class TagMapping
+    {
+        public TagDto MapTagInputModelToTagDto(TagInputModel model)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TagInputModel, TagDto>());
+            Mapper mapper = new Mapper(config);
+            TagDto tagDto = mapper.Map<TagDto>(model);
+            return tagDto;
+        }
+
+        public List<TagDto> MapTagInputModelToTagDto(List<TagInputModel> models)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TagInputModel, TagDto>());
+            Mapper mapper = new Mapper(config);
+            List<TagDto> tagDtos = mapper.Map<List<TagDto>>(models);
+            return tagDtos;
+        }
+    }
+}
