f652f612b360fdf3cb8f77c8ce6a71b9e5c0d980
Nastya <kushkoeva99@gmail.com>
2021-07-02 10:14:29 +0300

    add LessonDto

---
+++ DevEdu.API/DevEdu.DAL/Models/LessonDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEdu.DAL.Models
+{
+    class LessonDto : BaseDto
+    {
+        public DateTime Date { get; set; }
+        public CommentDto TeacherComment { get; set; }
+        public int TeacherId { get; set; }
+    }
+}
+
