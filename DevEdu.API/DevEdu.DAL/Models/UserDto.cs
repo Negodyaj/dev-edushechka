28e78d4508bb67b51eaa772c3f2e4b4dc72f1bd9
Eugene <Zhekul.90@gmail.com>
2021-07-02 05:28:21 +0300

    correct UserDto

--- DevEdu.API/DevEdu.DAL/Models/UserDto.cs
+++ DevEdu.API/DevEdu.DAL/Models/UserDto.cs
@@ -5,6 +5,7 @@ namespace DevEdu.DAL.Models
     public class UserDto : BaseDto
     {
         public string Name { get; set; }
+        public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public DateTime RegistrationDate { get; set; } 
