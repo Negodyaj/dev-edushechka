5912a371188f0095e6483f30dd5a53711db65c34
Eugene <Zhekul.90@gmail.com>
2021-07-02 04:18:21 +0300

    add UserDto

---
+++ DevEdu.API/DevEdu.DAL/Models/UserDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DevEdu.DAL.Models
+{
+    public class UserDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime RegistrationDate { get; set; } 
+        public string ContractNumber { get; set; }
+        public int CityId { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string GitHubAccount { get; set; }
+        public string Photo { get; set; }
+        public string PhoneNumer { get; set; }
+        public DateTime ExileDate { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as UserDto;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.Id.Equals(item.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
