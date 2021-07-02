e77b47d0e78999fb83a1eaea436ce18cc9341c94
Anton Efremenkov <anton@clearaccept.com>
2021-07-02 19:34:31 +0300

    changes for LessonController

--- DevEdu.API/DevEdu.API/Models/InputModels/AttendanceInputModel.cs
+++ DevEdu.API/DevEdu.API/Models/InputModels/AttendanceInputModel.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DevEdu.API.Models.InputModels
 {
-    public class StudentLessonUpdateFeedbackInputModel
+    public class AttendanceInputModel
     {
         [Required]
-        public string Feedback { get; set; }
+         public string IsPresent { get; set; }
+
     }
 }
