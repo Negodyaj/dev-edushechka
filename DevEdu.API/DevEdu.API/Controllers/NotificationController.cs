6accf651fb681a5b671a9772aa7aec77ecaa7ad7
Eugen <phlosof@gmail.com>
2021-07-04 20:27:53 +0300

    Add Notification controller, repo, dto, input model

---
+++ DevEdu.API/DevEdu.API/Controllers/NotificationController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using DevEdu.API.Models.InputModels;
+using DevEdu.DAL.Models;
+using DevEdu.DAL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevEdu.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationController
+    {
+        private readonly INotificationRepository _notificationRepository;
+        private readonly IMapper _mapper;
+        public NotificationController(IMapper mapper, INotificationRepository notificationRepository)
+        {
+            _notificationRepository = notificationRepository;
+            _mapper = mapper;
+        }
+
+        //  api/notification/5
+        [HttpGet("{id}")]
+        public NotificationDto GetNotification(int id)
+        {
+            return _notificationRepository.GetNotification(id);
+        }
+
+        //  api/notification/by-user/1
+        [HttpGet("by-user/{userId}")]
+        public List<NotificationDto> GetAllNotificationsByUserId(int userId)
+        {
+            return _notificationRepository.GetNotificationsByUser(userId);
+        }
+
+        //  api/notification
+        [HttpPost]
+        public int AddNotification([FromBody] NotificationAddInputModel model)
+        {
+            var dto = _mapper.Map<NotificationDto>(model);
+            return _notificationRepository.AddNotification(dto);
+        }
+
+        //  api/notification/5
+        [HttpDelete("{id}")]
+        public void DeleteNotification(int id)
+        {
+            _notificationRepository.DeleteNotification(id);
+        }
+
+        //  api/notification/5
+        [HttpPut("{id}")]
+        public string UpdateNotification(int id, [FromBody] NotificationUpdateInputModel model)
+        {
+            var dto = _mapper.Map<NotificationDto>(model);
+            _notificationRepository.UpdateNotification(id, dto);
+            return $"Text notification №{id} change to {model.Text}";
+        }
+    }
+}
+
---
+++ DevEdu.API/DevEdu.DAL/Repositories/NotificationRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using DevEdu.DAL.Models;
+namespace DevEdu.DAL.Repositories
+{
+    public class NotificationRepository : BaseRepository, INotificationRepository
+    {
+        public int AddNotification(NotificationDto notificationDto)
+        {
+            return _connection.QuerySingle<int>(
+                "dbo.Notification_Insert",
+                new
+                {
+                    notificationDto.UserId,
+                    notificationDto.Text
+                },
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public void DeleteNotification(int id)
+        {
+            _connection.Execute(
+                "dbo.Notification_Delete",
+                new { id },
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public NotificationDto GetNotification(int id)
+        {
+            return _connection.QuerySingle<NotificationDto>(
+                "dbo.Notification_SelectById",
+                new { id },
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public List<NotificationDto> GetNotificationsByUser(int userId)
+        {
+            return _connection
+                .Query<NotificationDto>(
+                    "dbo.Notification_SelectAllByUserId",
+                    new { userId },
+                    commandType: CommandType.StoredProcedure
+                )
+                .ToList();
+        }
+
+        public void UpdateNotification(int id, NotificationDto notificationDto)
+        {
+            _connection.Execute(
+                "dbo.Notification_Update",
+                new
+                {
+                    id,
+                    notificationDto.Text
+                },
+                commandType: CommandType.StoredProcedure
+            );
+        }
+    }
+}
