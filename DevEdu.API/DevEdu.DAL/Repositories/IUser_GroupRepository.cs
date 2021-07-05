90efbbbb3b9329574427224eccef1ee9bb9bbb12
Vadim <vki-13@yandex.ru>
2021-07-05 23:36:07 +0300

    Refactoring

---
+++ DevEdu.API/DevEdu.DAL/Repositories/IUser_GroupRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace DevEdu.DAL.Repositories
+{
+    public interface IUser_GroupRepository
+    {
+        void AddTag(int groupId, int userId, int roleId);
+        void DeleteTag(int userId, int groupId);
+    }
+}
--- DevEdu.API/DevEdu.DAL/Repositories/User_GroupRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/User_GroupRepository.cs
@@ -1,23 +1,19 @@
 ﻿using Dapper;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevEdu.DAL.Repositories
 {
-    public class User_GroupRepository: BaseRepository
+    public class User_GroupRepository : BaseRepository, IUser_GroupRepository
     {
+        private const string User_GroupInsertProcedure = "[dbo].[User_Group_Insert]";
+        private const string User_GroupDeleteProcedure = "[dbo].[Tag_Delete]";
         public void AddTag(int groupId, int userId, int roleId)
         {
-            _dbConnection.Execute("[dbo].[User_Group_Insert]", new { groupId, userId, roleId }, commandType: CommandType.StoredProcedure);
+            _connection.Execute(User_GroupInsertProcedure, new { groupId, userId, roleId }, commandType: CommandType.StoredProcedure);
         }
         public void DeleteTag(int userId, int groupId)
         {
-            _dbConnection.Execute("[dbo].[Tag_Delete]", new { userId, groupId }, commandType: CommandType.StoredProcedure);
+            _connection.Execute(User_GroupDeleteProcedure, new { userId, groupId }, commandType: CommandType.StoredProcedure);
         }
     }
 }
