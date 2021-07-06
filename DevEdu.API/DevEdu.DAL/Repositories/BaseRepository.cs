8a9512017f984bd07de0e289dded1bc37e3469cf
MariaMalyutina <merymal2696@gmail.com>
2021-07-06 18:53:08 +0300

    changed connection string

--- DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
@@ -5,9 +5,8 @@ namespace DevEdu.DAL.Repositories
 {
     public class BaseRepository
     {
-        //protected const string _connectionString = "Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist " +
-        //       "Security Info=True;User ID = student;Password=qwe!23";
-        protected const string _connectionString = "Server=localhost;Database=DevEdu;Trusted_Connection=True;";
+        protected const string _connectionString = "Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist " +
+               "Security Info=True;User ID = student;Password=qwe!23";
 
         protected readonly IDbConnection _connection;
 
