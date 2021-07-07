a38a9f2228b459d67d3b9de65f4e4f7dc7ea8ce9
Anton Efremenkov <anton@clearaccept.com>
2021-07-07 15:02:20 +0300

    fix after fix

--- DevEdu.API/DevEdu.DAL/Repositories/GroupRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/GroupRepository.cs
@@ -4,8 +4,8 @@ using DevEdu.DAL.Repositories;
 
 public class GroupRepository : BaseRepository, IGroupRepository
 {
-    private const string _userGroupInsertProcedure = "[dbo].[User_Group_Insert]";
-    private const string _userGroupDeleteProcedure = "[dbo].[Tag_Delete]";
+    private const string _userGroupInsertProcedure = "dbo.User_Group_Insert";
+    private const string _userGroupDeleteProcedure = "dbo.Tag_Delete";
     public const string _insertGroupLesson = "dbo.Group_Lesson_Insert";
     public const string _deleteGroupLesson = "dbo.Group_Lesson_Delete";
     public const string _insertGroupMaterial = "dbo.Group_Material_Insert";
@@ -13,22 +13,28 @@ public class GroupRepository : BaseRepository, IGroupRepository
 
     public void AddGroupLesson(int groupId, int lessonId)
     {
-        _connection.Execute(_insertGroupLesson, new
-        {
-            groupId,
-            lessonId
-        },
-             commandType: CommandType.StoredProcedure);
+        _connection.Execute(
+            _insertGroupLesson,
+            new
+            {
+                groupId,
+                lessonId
+            },
+            commandType: CommandType.StoredProcedure
+        );
     }
 
     public void RemoveGroupLesson(int groupId, int lessonId)
     {
-        _connection.Execute(_deleteGroupLesson, new
-        {
-            groupId,
-            lessonId
-        },
-            commandType: CommandType.StoredProcedure);
+        _connection.Execute(
+            _deleteGroupLesson, 
+            new
+            {
+                groupId,
+                lessonId
+            },
+            commandType: CommandType.StoredProcedure
+        );
     }
 
     public void AddGroupMaterialReference(int materialId, int groupId)
@@ -59,26 +65,26 @@ public class GroupRepository : BaseRepository, IGroupRepository
     public int AddUserToGroup(int groupId, int userId, int roleId)
     {
         return _connection.Execute(
-                    _userGroupInsertProcedure,
-                    new
-                    {
-                        groupId,
-                        userId,
-                        roleId
-                    },
-                    commandType: CommandType.StoredProcedure
-               );
+            _userGroupInsertProcedure,
+            new
+            {
+                groupId,
+                userId,
+                roleId
+            },
+            commandType: CommandType.StoredProcedure
+        );
     }
     public int DeleteUserFromGroup(int userId, int groupId)
     {
         return _connection.Execute(
-                    _userGroupDeleteProcedure,
-                    new
-                    {
-                        userId,
-                        groupId
-                    },
-                    commandType: CommandType.StoredProcedure
-               );
+            _userGroupDeleteProcedure,
+            new
+            {
+                userId,
+                groupId
+            },
+            commandType: CommandType.StoredProcedure
+        );
     }
 }
