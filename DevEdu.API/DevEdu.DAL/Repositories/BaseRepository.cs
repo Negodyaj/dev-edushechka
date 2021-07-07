a55bc879d32d2b4df1aefdc151be848445df22b3
Nastya <kushkoeva99@gmail.com>
2021-07-07 03:26:22 +0300

    add const connection string

--- DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
@@ -8,7 +8,7 @@ namespace DevEdu.DAL.Repositories
 {
     public class BaseRepository
     {
-        protected static string _connectionString = "Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23; Pooling=False;MultipleActiveResultSets=False;Connect Timeout = 60; Encrypt=False;TrustServerCertificate=False";
+        protected const string _connectionString = "Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23; Pooling=False;MultipleActiveResultSets=False;Connect Timeout = 60; Encrypt=False;TrustServerCertificate=False";
         protected readonly IDbConnection _connection;
 
         public BaseRepository()
