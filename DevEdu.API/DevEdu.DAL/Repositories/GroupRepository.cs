3131971cc83dfaac0e58f256c3cf4aec1961f6df
Vadim <vki-13@yandex.ru>
2021-07-07 13:39:01 +0300

    fix after merge

--- DevEdu.API/DevEdu.DAL/Repositories/GroupRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/GroupRepository.cs
@@ -4,52 +4,27 @@ using DevEdu.DAL.Repositories;
 
 public class GroupRepository : BaseRepository, IGroupRepository
 {
+    private const string _userGroupInsertProcedure = "[dbo].[User_Group_Insert]";
+    private const string _userGroupDeleteProcedure = "[dbo].[Tag_Delete]";
     public const string _insertGroupLesson = "dbo.Group_Lesson_Insert";
     public const string _deleteGroupLesson = "dbo.Group_Lesson_Delete";
     public const string _insertGroupMaterial = "dbo.Group_Material_Insert";
     public const string _deleteGroupMaterial = "dbo.Group_Material_Delete";
 
-    public  void AddGroupLesson(int groupId, int lessonId)
+    public void AddGroupLesson(int groupId, int lessonId)
     {
-       _connection.Execute(_insertGroupLesson, new
-            {
-                groupId,
-                lessonId
-            },
-            commandType: CommandType.StoredProcedure);
+        _connection.Execute(_insertGroupLesson, new
+        {
+            groupId,
+            lessonId
+        },
+             commandType: CommandType.StoredProcedure);
     }
-        private const string _userGroupInsertProcedure = "[dbo].[User_Group_Insert]";
-        private const string _userGroupDeleteProcedure = "[dbo].[Tag_Delete]";
-        public int AddUserToGroup(int groupId, int userId, int roleId)
-        {
-            return _connection.Execute(
-                        _userGroupInsertProcedure,
-                        new 
-                        { 
-                            groupId, 
-                            userId, 
-                            roleId 
-                        }, 
-                        commandType: CommandType.StoredProcedure
-                   );
-        }
-        public int DeleteUserFromGroup(int userId, int groupId)
-        {
-            return _connection.Execute(
-                        _userGroupDeleteProcedure, 
-                        new 
-                        { 
-                            userId, 
-                            groupId 
-                        }, 
-                        commandType: CommandType.StoredProcedure
-                   );
-        }
 
     public void RemoveGroupLesson(int groupId, int lessonId)
     {
         _connection.Execute(_deleteGroupLesson, new
-            {
+        {
             groupId,
             lessonId
         },
@@ -80,5 +55,30 @@ public class GroupRepository : BaseRepository, IGroupRepository
             },
             commandType: CommandType.StoredProcedure
         );
+    }
+    public int AddUserToGroup(int groupId, int userId, int roleId)
+    {
+        return _connection.Execute(
+                    _userGroupInsertProcedure,
+                    new
+                    {
+                        groupId,
+                        userId,
+                        roleId
+                    },
+                    commandType: CommandType.StoredProcedure
+               );
+    }
+    public int DeleteUserFromGroup(int userId, int groupId)
+    {
+        return _connection.Execute(
+                    _userGroupDeleteProcedure,
+                    new
+                    {
+                        userId,
+                        groupId
+                    },
+                    commandType: CommandType.StoredProcedure
+               );
     }
 }
--- DevEdu.API/DevEdu.DAL/Repositories/IGroupRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/IGroupRepository.cs
@@ -4,6 +4,8 @@
     {
         int AddUserToGroup(int groupId, int userId, int roleId);
         int DeleteUserFromGroup(int userId, int groupId);
+        void AddGroupLesson(int groupId, int lessonId);
+        void RemoveGroupLesson(int groupId, int lessonId);
         void AddGroupMaterialReference(int materialId, int groupId);
         void RemoveGroupMaterialReference(int materialId, int groupId);
     }
