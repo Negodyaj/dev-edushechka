2b6615502e55deeccfc8f126ad8c2af5674a6d3d
Eugene <Zhekul.90@gmail.com>
2021-07-09 02:41:49 +0300

    add & connect UserService

--- DevEdu.API/DevEdu.API/Startup.cs
+++ DevEdu.API/DevEdu.API/Startup.cs
@@ -1,4 +1,5 @@
 using DevEdu.Business.Services;
+using DevEdu.Business.Servicies;
 using DevEdu.DAL.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -36,7 +37,8 @@ namespace DevEdu.API
             services.AddScoped<IGroupService, GroupService>();
             services.AddScoped<IMaterialService, MaterialService>();
             services.AddScoped<ITaskService, TaskService>();
-            services.AddScoped<ICourseService, CourseService>(); 
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<IUserService, UserService>();
 
             services.AddControllers();
 
