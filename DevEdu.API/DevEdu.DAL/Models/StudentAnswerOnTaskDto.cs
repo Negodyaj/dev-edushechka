2eba1dc846730e4744b8b933b4fd4818847322b8
Vadim <vadimsanduca@gmail.com>
2021-07-08 14:41:09 +0300

    AddListsOfDtosToTaskDto

---
+++ DevEdu.API/DevEdu.DAL/Models/StudentAnswerOnTaskDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEdu.DAL.Models
+{
+    public class StudentAnswerOnTaskDto
+    {
+        public int TaskId { get; set; }
+        public int StudentId { get; set; }
+        public int StatusId { get; set; }
+        public string Answer { get; set; }
+    }
+}
+
