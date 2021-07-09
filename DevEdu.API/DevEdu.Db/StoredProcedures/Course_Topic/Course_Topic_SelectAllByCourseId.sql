38e318b2f5c9982c7ec805408639f973eb019ea6
Katerina <k.panteleewa@gmail.com>
2021-07-09 17:25:01 +0300

    RHB-10: Added new stored procedure Course_Topic_SelectAllByCourseId

---
+++ DevEdu.API/DevEdu.Db/StoredProcedures/Course_Topic/Course_Topic_SelectAllByCourseId.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Course_Topic_SelectAllByCourseId]
+	@CourseId int
+As
+Begin
+	Select 
+		ct.TopicId,
+		ct.Position,
+		t.Name as TopicName,
+		t.Duration
+	From dbo.Course_Topic ct
+		inner join dbo.Topic t on t.Id = ct.TopicId 
+	Where (CourseId = @CourseId)
+End
