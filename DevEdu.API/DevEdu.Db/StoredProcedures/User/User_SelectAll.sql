aeb23aea761dcadf75c4087c84eec2d5a0292d8e
Eugene <Zhekul.90@gmail.com>
2021-07-09 21:44:26 +0300

    RHB-5: correct User stored procedures

--- DevEdu.API/DevEdu.Db/StoredProcedures/User/User_SelectAll.sql
+++ DevEdu.API/DevEdu.Db/StoredProcedures/User/User_SelectAll.sql
@@ -2,21 +2,17 @@
 AS
 BEGIN
 	SELECT 
-		FirstName,
-		LastName,
-		Patronymic,
-		Email,
-		Username,
-		Password,
-		IsDeleted,
-		RegistrationDate,
-		ContractNumber,
-		CityId,
-		BirthDate,
-		GitHubAccount,
-		Photo,
-		PhoneNumber,
-		ExileDate
-	FROM dbo.[User]
+		u.FirstName,
+		u.LastName,
+		u.Email,
+		u.IsDeleted,
+		u.RegistrationDate,
+		u.ContractNumber,
+		u.Photo,
+		u.PhoneNumber,
+		c.Id,
+		c.Name
+	FROM dbo.[User] u
+	left join dbo.[City] c on u.CityId=c.Id
 	WHERE IsDeleted = 0
 END
