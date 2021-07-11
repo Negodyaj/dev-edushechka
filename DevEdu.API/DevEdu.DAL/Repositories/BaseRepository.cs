b8429b8f35b0221da0078d6031161a24a95481de
Eugene <Zhekul.90@gmail.com>
2021-07-11 15:10:19 +0300

    RHB-5: return correct connectionString

--- DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
@@ -5,11 +5,8 @@ namespace DevEdu.DAL.Repositories
 {
     public abstract class BaseRepository
     {
-        //protected const string _connectionString =
-        //    @"Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;";
-
-        protected const string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DevEdu;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
-
+        protected const string _connectionString =
+            @"Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;";
 
         protected IDbConnection _connection;
 
