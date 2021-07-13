52ecc32961aeda96f56af4c60d42c269abc9cdff
Eugene <Zhekul.90@gmail.com>
2021-07-13 05:34:02 +0300

    RHB-20: add description & producesResponseType

--- DevEdu.API/DevEdu.API/Controllers/UserController.cs
+++ DevEdu.API/DevEdu.API/Controllers/UserController.cs
@@ -3,8 +3,10 @@ using DevEdu.API.Models.InputModels;
 using DevEdu.API.Models.OutputModels;
 using DevEdu.Business.Servicies;
 using DevEdu.DAL.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace DevEdu.API.Controllers
 {
@@ -23,6 +25,8 @@ namespace DevEdu.API.Controllers
 
         // api/user
         [HttpPost]
+        [Description("Add new user")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
         public int AddUser([FromBody] UserInsertInputModel model)
         {
             var dto = _mapper.Map<UserDto>(model);
@@ -31,6 +35,8 @@ namespace DevEdu.API.Controllers
 
         // api/user/userId
         [HttpPut("{userId}")]
+        [Description("Update user")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public void UpdateUserById([FromBody] UserUpdateInputModel model)
         {
             var dto = _mapper.Map<UserDto>(model);
@@ -39,6 +45,8 @@ namespace DevEdu.API.Controllers
 
         // api/user/{userId}
         [HttpGet("{userId}")]
+        [Description("Return user by id")]
+        [ProducesResponseType(typeof(UserFullInfoOutPutModel), StatusCodes.Status200OK)]
         public UserFullInfoOutPutModel GetUserById(int userId)
         {
             var dto = _userService.SelectUserById(userId);
@@ -47,6 +55,8 @@ namespace DevEdu.API.Controllers
 
         // api/user
         [HttpGet]
+        [Description("Return list users")]
+        [ProducesResponseType(typeof(List<UserInfoOutPutModel>), StatusCodes.Status200OK)]
         public List<UserInfoOutPutModel> GetAllUsers()
         {
             var listDto = _userService.SelectUsers();
@@ -55,6 +65,8 @@ namespace DevEdu.API.Controllers
 
         // api/user/{userId}
         [HttpDelete("{userId}")]
+        [Description("Delete user by id")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public void DeleteUser(int userId)
         {
             _userService.DeleteUser(userId);
@@ -62,6 +74,8 @@ namespace DevEdu.API.Controllers
 
         // api/user/{userId}/role/{roleId}
         [HttpPost("{userId}/role/{roleId}")]
+        [Description("Add new role to user")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
         public int AddRoleToUser(int userId, int roleId)
         {
             return _userService.AddUserRole(userId, roleId);
@@ -69,6 +83,8 @@ namespace DevEdu.API.Controllers
 
         // api/user/{userId}/role/{roleId}
         [HttpDelete("{userId}/role/{roleId}")]
+        [Description("Delete role from user")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public void DeleteRoleFromUser(int userId, int roleId)
         {
             _userService.DeleteUserRole(userId, roleId);
