f931b5daf1123690541ea5ae794b0aeab11fee9b
Eugene <Zhekul.90@gmail.com>
2021-07-14 21:27:52 +0300

    RHB-20: correct tests

--- DevEdu.API/DevEdu.Business.Tests/UserServiceTests.cs
+++ DevEdu.API/DevEdu.Business.Tests/UserServiceTests.cs
@@ -13,15 +13,19 @@ namespace DevEdu.Business.Tests.Services
     {
 
         private Mock<IUserRepository> _userRepoMock;
-        private UserDto _userDto;
-        private UserService _userService;
 
         [SetUp]
         public void Setup()
         {
             _userRepoMock = new Mock<IUserRepository>();
-            _userService = new UserService(_userRepoMock.Object);
-            _userDto = new UserDto
+        }
+
+        [Test]
+        public void AddUser_DtoWithRoles_UserDtoWithRolesCreated()
+        {
+            //Given
+            var expectedUserId = 1;
+            var _userDto = new UserDto
             {
                 FirstName = "Admin",
                 LastName = "Adminov",
@@ -41,87 +45,54 @@ namespace DevEdu.Business.Tests.Services
                      Role.Admin
                 }
             };
-        }
-
-        [Test]
-        public void AddUser_DtoWithRoles_UserDtoWithRolesCreated()
-        {
-            //Given
-            var expectedUserId = 1;
-
-            _userRepoMock.Setup(x => x.AddUser(_userDto))
-                .Returns(expectedUserId)
-                .Verifiable();
+            _userRepoMock.Setup(x => x.AddUser(_userDto)).Returns(expectedUserId);
+            _userRepoMock.Setup(x => x.AddUserRole(expectedUserId, It.IsAny<int>()));
+            var sut = new UserService(_userRepoMock.Object);
 
             //When
-            var actualUserId = _userService.AddUser(_userDto);
+            var actualUserId = sut.AddUser(_userDto);
 
             //Then
             Assert.AreEqual(expectedUserId, actualUserId);
             _userRepoMock.Verify(x => x.AddUser(_userDto), Times.Once);
-        }
-
-        [Test]
-        public void AddUser_WhenDtoIsEmpty_Exception()
-        {
-            //Given
-            UserDto emptyUserDto = null;
-            _userRepoMock.Setup(x => x.AddUser(emptyUserDto))
-                .Throws(new Exception());
-
-            //When
-            try
-            {
-                _userService.AddUser(emptyUserDto);
-            }
-            catch
-            {
-                Assert.Pass();
-            }
-
-            //Then
-            Assert.Fail();
+            _userRepoMock.Verify(x => x.AddUserRole(actualUserId, It.IsAny<int>()), Times.Exactly(_userDto.Roles.Count));
         }
 
         [Test]
         public void SelectById_IntUserId_ReturnUserDto()
         {
             //Given
+            var _userDto = new UserDto
+            {
+                FirstName = "Admin",
+                LastName = "Adminov",
+                Patronymic = "Adminovich",
+                Email = "admin@admin.ad",
+                Username = "Admin01",
+                Password = "qwerty12345",
+                ContractNumber = "admin01",
+                City = (City)1,
+                BirthDate = DateTime.Today,
+                GitHubAccount = "admin/admin.git",
+                Photo = "https://localhost:Admin/admin",
+                PhoneNumber = "adminPhoneNumber",
+                Roles = new List<Role>
+                {
+                     Role.Student ,
+                     Role.Admin
+                }
+            };
             var userId = 1;
-
             _userRepoMock.Setup(x => x.SelectUserById(userId))
-                .Returns(_userDto)
-                .Verifiable();
+                .Returns(_userDto);
+            var sut = new UserService(_userRepoMock.Object);
 
             //When
-            var dto = _userService.SelectUserById(userId);
+            var dto = sut.SelectUserById(userId);
 
             //Then
             Assert.AreEqual(_userDto, dto);
             _userRepoMock.Verify(x => x.SelectUserById(userId), Times.Once);
-        }
-
-        [TestCase(0)]
-        [TestCase(-10)]
-        [TestCase(-int.MaxValue)]
-        public void SelectById_WhenUserIdWrongValue_Exception(int userId)
-        {
-            //Given
-            _userRepoMock.Setup(x => x.SelectUserById(userId))
-                .Throws(new Exception());
-
-            //When
-            try
-            {
-                _userService.SelectUserById(userId);
-            }
-            catch
-            {
-                Assert.Pass();
-            }
-
-            //Then
-            Assert.Fail();
         }
 
         [Test]
@@ -130,7 +101,26 @@ namespace DevEdu.Business.Tests.Services
             //Given
             var expectedListUserDto = new List<UserDto>
             {
-                _userDto,
+                new UserDto
+                {
+                    FirstName = "Admin",
+                    LastName = "Adminov",
+                    Patronymic = "Adminovich",
+                    Email = "admin@admin.ad",
+                    Username = "Admin01",
+                    Password = "qwerty12345",
+                    ContractNumber = "admin01",
+                    City = (City)1,
+                    BirthDate = DateTime.Today,
+                    GitHubAccount = "admin/admin.git",
+                    Photo = "https://localhost:Admin/admin",
+                    PhoneNumber = "adminPhoneNumber",
+                    Roles = new List<Role>
+                    {
+                         Role.Student ,
+                         Role.Admin
+                    }
+                },
                 new UserDto
                 {
                     FirstName = "Student",
@@ -164,13 +154,12 @@ namespace DevEdu.Business.Tests.Services
                     Roles = new List<Role> { Role.Manager }
                 }
             };
-
             _userRepoMock.Setup(x => x.SelectUsers())
-                .Returns(expectedListUserDto)
-                .Verifiable();
+                .Returns(expectedListUserDto);
+            var sut = new UserService(_userRepoMock.Object);
 
             //When
-            var actualListUserDto = _userService.SelectUsers();
+            var actualListUserDto = sut.SelectUsers();
 
             //Then
             Assert.AreEqual(expectedListUserDto, actualListUserDto);
@@ -181,6 +170,26 @@ namespace DevEdu.Business.Tests.Services
         public void UpdateUser_UserDto_ReturnUpdateUserDto()
         {
             //Given
+            var _userDto = new UserDto
+            {
+                FirstName = "Admin",
+                LastName = "Adminov",
+                Patronymic = "Adminovich",
+                Email = "admin@admin.ad",
+                Username = "Admin01",
+                Password = "qwerty12345",
+                ContractNumber = "admin01",
+                City = (City)1,
+                BirthDate = DateTime.Today,
+                GitHubAccount = "admin/admin.git",
+                Photo = "https://localhost:Admin/admin",
+                PhoneNumber = "adminPhoneNumber",
+                Roles = new List<Role>
+                {
+                     Role.Student ,
+                     Role.Admin
+                }
+            };
             var expectedUserDto = new UserDto
             {
                 FirstName = "Student",
@@ -197,13 +206,12 @@ namespace DevEdu.Business.Tests.Services
                 PhoneNumber = "StudentPhoneNumber",
                 Roles = new List<Role> { Role.Student }
             };
-
-            _userRepoMock.Setup(x => x.UpdateUser(_userDto))
-                .Returns(expectedUserDto)
-                .Verifiable();
+            _userRepoMock.Setup(x => x.UpdateUser(_userDto));
+            _userRepoMock.Setup(x => x.SelectUserById(_userDto.Id)).Returns(expectedUserDto);
+            var sut = new UserService(_userRepoMock.Object);
 
             //When
-            var actualUserDto = _userService.UpdateUser(_userDto);
+            var actualUserDto = sut.UpdateUser(_userDto);
 
             //Then
             Assert.AreEqual(expectedUserDto, actualUserDto);
@@ -211,111 +219,39 @@ namespace DevEdu.Business.Tests.Services
         }
 
         [Test]
-        public void UpdateUser_WhenDtoIsEmpty_Exception()
-        {
-            //Given
-            UserDto emptyDto = null;
-            _userRepoMock.Setup(x => x.UpdateUser(emptyDto))
-                .Throws(new Exception());
-
-            //When
-            try
-            {
-                _userService.UpdateUser(emptyDto);
-            }
-            catch
-            {
-                Assert.Pass();
-            }
-
-            //Then
-            Assert.Fail();
-        }
-
-        [TestCase(0)]
-        [TestCase(-10)]
-        [TestCase(-int.MaxValue)]
-        public void DeleteUser_WrongValue_Exception(int userId)
-        {
-            //Given
-            _userRepoMock.Setup(x => x.DeleteUser(userId))
-                .Throws(new Exception());
-
-            //When
-            try
-            {
-                _userService.DeleteUser(userId);
-            }
-            catch
-            {
-                Assert.Pass();
-            }
-
-            //Then
-            Assert.Fail();
-        }
-
-        [Test]
         public void AddRoleToUser_IntUserIdAndRoleId_ReturnIntRecord()
         {
             //Given
             var expectedUserRoleId = 24;
-            _userRepoMock.Setup(x => x.AddUserRole(_userDto.Id, (int)Role.Teacher))
-                .Returns(expectedUserRoleId)
-                .Verifiable();
+            var _userDto = new UserDto
+            {
+                FirstName = "Admin",
+                LastName = "Adminov",
+                Patronymic = "Adminovich",
+                Email = "admin@admin.ad",
+                Username = "Admin01",
+                Password = "qwerty12345",
+                ContractNumber = "admin01",
+                City = (City)1,
+                BirthDate = DateTime.Today,
+                GitHubAccount = "admin/admin.git",
+                Photo = "https://localhost:Admin/admin",
+                PhoneNumber = "adminPhoneNumber",
+                Roles = new List<Role>
+                {
+                     Role.Student ,
+                     Role.Admin
+                }
+            };
+            _userRepoMock.Setup(x => x.AddUserRole(_userDto.Id, (int)Role.Teacher)).Returns(expectedUserRoleId);
+            var sut = new UserService(_userRepoMock.Object);
 
             //When
-            var actualUserRoleId = _userService.AddUserRole(_userDto.Id, (int)Role.Teacher);
+            var actualUserRoleId = sut.AddUserRole(_userDto.Id, (int)Role.Teacher);
 
             //Then
             Assert.AreEqual(expectedUserRoleId, actualUserRoleId);
             _userRepoMock.Verify(x => x.AddUserRole(_userDto.Id, (int)Role.Teacher), Times.Once);
-        }
-
-        [TestCase(0,1)]
-        [TestCase(1,0)]
-        [TestCase(-int.MaxValue,-int.MaxValue)]
-        public void AddRoleToUser_WrongValue_Exception(int userId,int roleId)
-        {
-            //Given
-            _userRepoMock.Setup(x => x.AddUserRole(userId, roleId))
-                .Throws(new Exception());
-
-            //When
-            try
-            {
-                _userService.AddUserRole(userId, roleId);
-            }
-            catch
-            {
-                Assert.Pass();
-            }
-
-            //Then
-            Assert.Fail();
-        }
-
-        [TestCase(0, 1)]
-        [TestCase(1, 0)]
-        [TestCase(-int.MaxValue, -int.MaxValue)]
-        public void DeleteUserRole_WrongValue_Exception(int userId, int roleId)
-        {
-            //Given
-            _userRepoMock.Setup(x => x.DeleteUserRole(userId, roleId))
-                .Throws(new Exception());
-
-            //When
-            try
-            {
-                _userService.DeleteUserRole(userId, roleId);
-            }
-            catch
-            {
-                Assert.Pass();
-            }
-
-            //Then
-            Assert.Fail();
         }
     }
 }
