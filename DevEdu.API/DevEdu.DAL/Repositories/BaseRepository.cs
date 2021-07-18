a321c0a6ba5a513f6cdd61f7caf63688d8147e33
Eugen <phlosof@gmail.com>
2021-07-18 04:52:07 +0300

    fix

--- DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
@@ -6,8 +6,8 @@ namespace DevEdu.DAL.Repositories
     public abstract class BaseRepository
     {
         protected const string _connectionString =
-          // @"Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;";
-          @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=localTest;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
+          @"Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;";
+
         protected IDbConnection _connection;
 
         protected BaseRepository()
