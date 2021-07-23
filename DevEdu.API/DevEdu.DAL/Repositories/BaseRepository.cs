7a37fbf7829c2f7327c57d0944c034f98ad79b4f
Timur <Kotafrakt@gmail.com>
2021-07-23 22:16:55 +0300

    fix string

--- DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
+++ DevEdu.API/DevEdu.DAL/Repositories/BaseRepository.cs
@@ -5,8 +5,8 @@ namespace DevEdu.DAL.Repositories
 {
     public abstract class BaseRepository
     {
-        protected const string _connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=DevEdu.Db;";
-          //@"Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;";
+        protected const string _connectionString = 
+          @"Data Source=80.78.240.16;Initial Catalog = DevEdu; Persist Security Info=True;User ID = student;Password=qwe!23;";
 
         protected IDbConnection _connection;
 
