a760e15e0a860a272424840fa3e10d0e1f2ad899
Katerina <k.panteleewa@gmail.com>
2021-07-23 03:21:49 +0300

    RHB-37: Added some tests for CourseTopics

--- DevEdu.API/DevEdu.Business.Tests/CourseData.cs
+++ DevEdu.API/DevEdu.Business.Tests/CourseData.cs
@@ -35,31 +35,45 @@ namespace DevEdu.Business.Tests
 
             return courseTopicDto;
         }
-        /// <summary> This method returns List CourseTopicDto, you can choose 1-3 version.</summary>
+        /// <summary> This method returns List CourseTopicDto, you can choose 1-5 version.</summary>
         public static List<CourseTopicDto> GetListCourseTopicDto(int vesionOfList)
         {
             List<CourseTopicDto> courseTopicsDto = new List<CourseTopicDto>();
             switch (vesionOfList)
             {
                 case 1:
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 5 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 6 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 8 });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 5, Id = 1, Topic = new TopicDto { Id = 1 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 6, Id = 2, Topic = new TopicDto { Id = 2 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 8, Id = 3, Topic = new TopicDto { Id = 3 } });
                     break;
                 case 2:
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 1 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 3 });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 1, Id = 3, Topic = new TopicDto { Id = 3 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 3, Id = 6, Topic = new TopicDto { Id = 6 } });
                     break;
                 case 3:
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 1 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 3 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 6 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 8 });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 1, Id = 8, Topic = new TopicDto { Id = 8 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 3, Id = 6, Topic = new TopicDto { Id = 6 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 6, Id = 9, Topic = new TopicDto { Id = 9 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 8, Id = 2, Topic = new TopicDto { Id = 2 } });
+                    break;
+                case 4:
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 4, Id = 15, Topic = new TopicDto { Id = 15 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 2, Id = 21, Topic = new TopicDto { Id = 21 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 1, Id = 13, Topic = new TopicDto { Id = 13 } });
+                    break;
+                case 5:
+                    courseTopicsDto = new List<CourseTopicDto>();
+                    break;
+
+                case 6:
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 4, Id = 15, Topic = new TopicDto { Id = 15 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 4, Id = 21, Topic = new TopicDto { Id = 21 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 1, Id = 13, Topic = new TopicDto { Id = 13 } });
                     break;
                 default:
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 5 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 3 });
-                    courseTopicsDto.Add(new CourseTopicDto { Position = 8 });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 5, Id = 7, Topic = new TopicDto { Id = 7 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 3, Id = 11, Topic = new TopicDto { Id = 11 } });
+                    courseTopicsDto.Add(new CourseTopicDto { Position = 8, Id = 12, Topic = new TopicDto { Id = 12 } });
                     break;
             }
             return courseTopicsDto;
--- DevEdu.API/DevEdu.Business.Tests/CourseServiceTests.cs
+++ DevEdu.API/DevEdu.Business.Tests/CourseServiceTests.cs
@@ -80,6 +80,73 @@ namespace DevEdu.Business.Tests
             _courseRepositoryMock.Verify(x => x.SelectAllTopicsByCourseId(givenCourseId), Times.Once);
 
         }
+        [Test]
+        public void UpdateCourseTopicsByCourseId_WithSameAmountTopics_Updated()
+        {
+            //Given
+            var givenCourseId = 7;
+            var givenTopicsToUpdate = CourseData.GetListCourseTopicDto(1);
+            var toicsFromDB = CourseData.GetListCourseTopicDto(4); 
+            _courseRepositoryMock.Setup(x => x.SelectAllTopicsByCourseId(givenCourseId)).Returns(toicsFromDB);
+            _courseRepositoryMock.Setup(x => x.UpdateCourseTopicsByCourseId(givenTopicsToUpdate));
+            var sut = new CourseService(_topicRepositoryMock.Object, _courseRepositoryMock.Object);
+            //When
+            sut.UpdateCourseTopicsByCourseId(givenCourseId, givenTopicsToUpdate);
+            //Then
+            _courseRepositoryMock.Verify(x => x.UpdateCourseTopicsByCourseId(givenTopicsToUpdate), Times.Once);
+
+        }
+        [Test]
+        public void UpdateCourseTopicsByCourseId_WithDifferentAmountTopics_Updated()
+        {
+            //Given
+            var givenCourseId = 7;
+            var givenTopicsToUpdate = CourseData.GetListCourseTopicDto(1);
+            var toicsFromDB = CourseData.GetListCourseTopicDto(2);
+            _courseRepositoryMock.Setup(x => x.SelectAllTopicsByCourseId(givenCourseId)).Returns(toicsFromDB);
+            _courseRepositoryMock.Setup(x => x.UpdateCourseTopicsByCourseId(givenTopicsToUpdate));
+            var sut = new CourseService(_topicRepositoryMock.Object, _courseRepositoryMock.Object);
+            //When
+            sut.UpdateCourseTopicsByCourseId(givenCourseId, givenTopicsToUpdate);
+            //Then
+            _courseRepositoryMock.Verify(x => x.DeleteAllTopicsByCourseId(givenCourseId), Times.Once);
+            _topicRepositoryMock.Verify(x => x.AddTopicsToCourse(givenTopicsToUpdate), Times.Once);
+        }
+        [Test]
+        public void UpdateCourseTopicsByCourseId_TopicsInDatabaseAreAbsentForCourse_AddedTopicsForCourse()
+        {
+            //Given
+            var givenCourseId = 3;
+            var givenTopicsToUpdate = CourseData.GetListCourseTopicDto(3);
+            var toicsFromDB = CourseData.GetListCourseTopicDto(5);
+            _courseRepositoryMock.Setup(x => x.SelectAllTopicsByCourseId(givenCourseId)).Returns(toicsFromDB);
+            _courseRepositoryMock.Setup(x => x.UpdateCourseTopicsByCourseId(givenTopicsToUpdate));
+            var sut = new CourseService(_topicRepositoryMock.Object, _courseRepositoryMock.Object);
+            //When
+            sut.UpdateCourseTopicsByCourseId(givenCourseId, givenTopicsToUpdate);
+            //Then
+            _topicRepositoryMock.Verify(x => x.AddTopicsToCourse(givenTopicsToUpdate), Times.Once);
+        }
+        [Test]
+        public void UpdateCourseTopicsByCourseId_TopicsCountIsZero_Returnerd()
+        {
+            //Given
+            var givenCourseId = 3;
+            var givenTopicsToUpdate = CourseData.GetListCourseTopicDto(5);
+            var toicsFromDB = CourseData.GetListCourseTopicDto(3);
+            _courseRepositoryMock.Setup(x => x.SelectAllTopicsByCourseId(givenCourseId)).Returns(toicsFromDB);
+            _courseRepositoryMock.Setup(x => x.UpdateCourseTopicsByCourseId(givenTopicsToUpdate));
+            var sut = new CourseService(_topicRepositoryMock.Object, _courseRepositoryMock.Object);
+            //When
+            sut.UpdateCourseTopicsByCourseId(givenCourseId, givenTopicsToUpdate);
+            //Then
+            _courseRepositoryMock.Verify(x => x.UpdateCourseTopicsByCourseId(givenTopicsToUpdate), Times.Never);
+        }
+        [Test]
+        protected void UpdateCourseTopicsByCourseId_PositionsNotUniqueness_ThrownException()
+        {
+
+        }
 
     }
 }
