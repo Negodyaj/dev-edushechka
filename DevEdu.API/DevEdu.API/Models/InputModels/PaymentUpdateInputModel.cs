d55614492f5662f172d05756c207cfe3179619e8
Katerina <k.panteleewa@gmail.com>
2021-07-26 21:10:52 +0300

    rhb-17 : added validation attribute

--- DevEdu.API/DevEdu.API/Models/InputModels/PaymentUpdateInputModel.cs
+++ DevEdu.API/DevEdu.API/Models/InputModels/PaymentUpdateInputModel.cs
@@ -4,12 +4,14 @@ using System.ComponentModel.DataAnnotations;
 using static DevEdu.API.Common.ValidationMessage;
 using System.Linq;
 using System.Threading.Tasks;
+using DevEdu.API.Common;
 
 namespace DevEdu.API.Models.InputModels
 {
     public class PaymentUpdateInputModel
     {
         [Required(ErrorMessage = DateRequired)]
+        [CustomDateFormatAttribute(ErrorMessage = WrongFormatDate)]
         public string Date { get; set; }
 
         [Required(ErrorMessage = SumRequired)]
