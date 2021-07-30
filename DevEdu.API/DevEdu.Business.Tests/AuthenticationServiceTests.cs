409998c161a0ebbe31e86b206d7a1a24b7917f84
Avaxapsys <vv.krugliy@gmail.com>
2021-07-30 10:16:55 +0300

    beutify

--- DevEdu.API/DevEdu.Business.Tests/AuthenticationServiceTests.cs
+++ DevEdu.API/DevEdu.Business.Tests/AuthenticationServiceTests.cs
@@ -20,10 +20,13 @@ namespace DevEdu.Business.Tests
         [Test]
         public void HashPassword_PasswordAndSalt_ReturnSalt()
         {
+            //Given 
             string password = "password";
             byte[] salt = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            var actual = _auth.HashPassword(password, salt);
             string expected = "AAECAwQFBgcICQoLDA0ODyT2cCjwnE2JIl0Ka2bvFeMtEwX+";
+            //When
+            var actual = _auth.HashPassword(password, salt);
+            //Than
             Assert.AreEqual(expected, actual);
         }
         [Test]
@@ -31,23 +34,27 @@ namespace DevEdu.Business.Tests
         {
             string password = "password";
             byte[] salt = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            
             Assert.Throws<ArgumentException>(() => _auth.HashPassword(password, salt));
         }
 
         [Test]
         public void Verify_CorrectPassword_GetTrue()
         {
+            //Given 
             var expected = true;
             string hashedPassword = "AAECAwQFBgcICQoLDA0ODyT2cCjwnE2JIl0Ka2bvFeMtEwX+";
             string userPassword = "password";
-
+            //When
             var actual = _auth.Verify(hashedPassword, userPassword);
+            //Than
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void Verify_IncorrectPassword_GetException()
         {
+            //Given 
             string hashedPassword = "AAECAwQFBgcICQoLDA0ODyT2cCjwnE2JIl0Ka2bvFeMtEwX";
             string userPassword = "password";
 
