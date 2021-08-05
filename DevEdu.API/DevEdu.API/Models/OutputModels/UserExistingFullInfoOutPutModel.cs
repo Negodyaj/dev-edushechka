0faaf23e2f660c68a9b2473cdd9c9e5e98a093fd
Eugene <Zhekul.90@gmail.com>
2021-08-05 14:12:57 +0300

    RHB-49: correct userOutput  model

--- DevEdu.API/DevEdu.API/Models/OutputModels/UserExistingFullInfoOutPutModel.cs
+++ DevEdu.API/DevEdu.API/Models/OutputModels/UserExistingFullInfoOutPutModel.cs
@@ -4,12 +4,14 @@ namespace DevEdu.API.Models.OutputModels
 {
     public class UserExistingFullInfoOutPutModel : UserInfoOutPutModel
     {
+        public string Patronymic { get; set; }
         public string Username { get; set; }
         public string RegistrationDate { get; set; }
         public string ContractNumber { get; set; }
         public string BirthDate { get; set; }
         public string GitHubAccount { get; set; }
         public string PhoneNumber { get; set; }
+        public string ExileDate { get; set; }
         public City City { get; set; }
     }
 }
