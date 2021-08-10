a64b1aae3d46c5be889899692dc8e7bb758ab875
Ira <brainforfreedom@gmail.com>
2021-08-10 19:46:06 +0300

    RHB-59 delete ValidationError

--- DevEdu.API/DevEdu.API/Configuration/ExceptionResponses/ValidationExceptionExtensionResponse.cs
+++ DevEdu.API/DevEdu.API/Configuration/ExceptionResponses/ValidationExceptionExtensionResponse.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using DevEdu.API.Configuration;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 
@@ -8,20 +9,20 @@ namespace DevEdu.Business.Exceptions
     {
         public int Code { get; set; }
         public string Message { get; set; }
-        public List<ValidationError> ValidationErrors { get; set; }
+        public List<ValidationExceptionResponse> ValidationErrors { get; set; }
 
         public ValidationExceptionExtensionResponse(ModelStateDictionary modelState)
         {
             Code = 1001;
             Message = "Validation Failed";
-            ValidationErrors = new List<ValidationError>();
+            ValidationErrors = new List<ValidationExceptionResponse>();
             foreach (var state in modelState)
             {
-                ValidationErrors.Add(new ValidationError
+                ValidationErrors.Add(new ValidationExceptionResponse
                 {
                     Code = 422,
-                    Field = $"{state.Key}",
-                    ErrorMessage = $"Invalid format {state.Value.Errors[0].ErrorMessage} "
+                    Message = $"{state.Key}",
+                    Description = $"Invalid format {state.Value.Errors[0].ErrorMessage} "
                 });
             }
         }
