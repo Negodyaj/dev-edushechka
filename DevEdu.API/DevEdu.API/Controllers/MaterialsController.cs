1d609210b5c5d498737d226c4245dd2db747187f
Anton Efremenkov <anton.efremenkov@dataart.com>
2022-05-11 18:12:22 +0300

    fix for route

--- DevEdu.API/DevEdu.API/Controllers/MaterialsController.cs
+++ DevEdu.API/DevEdu.API/Controllers/MaterialsController.cs
@@ -57,7 +57,7 @@ namespace DevEdu.API.Controllers
 
         // api/materials/5
         [AuthorizeRoles(Role.Methodist)]
-        [HttpPatch("{id}")]
+        [HttpPatch("{id}/restore")]
         [Description("Restore material by id")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status403Forbidden)]
